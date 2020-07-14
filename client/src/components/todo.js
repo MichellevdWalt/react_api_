100518572c937618a0efef630d483349cf8fcb2e
MichellevdWalt <michellevdwalt13@gmail.com>
2020-07-14 17:45:21 +0200

    todo updated

--- client/src/components/todo.js
+++ client/src/components/todo.js
@@ -1,14 +1,2 @@
-//TODO implement forbidden route
-//TODO CreateCourse todos
-//TODO tidy up, delete console.logs
-//TODO brush up comments
-//TODO validation errors UpdateCourse
-//TODO CourseDetail - materials list editing
+
 //TODO React markdown see https://www.npmjs.com/package/react-markdown
-//TODO auto redirect authentication page and add home button
-//TODO redirect create course to the detail page
-//TODO Figure out how to redirect to previous page
-
-//!BONUS
-
-//?Buttons like My Courses on home page
